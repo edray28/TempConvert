6be63b8024b45aaabddf4ac98c84435f27c4f730
edray28 <edraytuyor@gmail.com>
2021-03-29 18:04:58 +0800

    Added HTML and CSS Code only

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const cel_to_far = document.querySelector("#flexRadioDefault1");
+const far_to_cel = document.querySelector("#flexRadioDefault2");
+const input_temp = document.querySelector(".form-control");
+const convert_button = document.querySelector("#convert");
